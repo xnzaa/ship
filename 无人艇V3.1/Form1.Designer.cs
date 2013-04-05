d721bfb480bef67b54a85f0f24ff8dae8f0e8d90
99 <sherlock99@qq.com>
2013-04-05 11:48:32 +0800

    fixed some commits 2

--- 无人艇V3.1/Form1.Designer.cs
+++ 无人艇V3.1/Form1.Designer.cs
@@ -1,15 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
-using System.Net;
-using System.Net.Sockets;
-using System.Threading;
-using System.IO;
+﻿using System.Windows.Forms;
 
 namespace whut_ship_control
 {
